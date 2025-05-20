47f7d5a9b33914abc7dd64b1734f2e1952800710
mishashto <me@reze.moe>
2025-05-20 19:26:05 +0900

    chore: change configurations to fit development cycle

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Phantasia",
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+      meta: [
+        { name: 'description', content: 'Heavily opinionated image and file server.' }
+      ],
+    }
+  },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
