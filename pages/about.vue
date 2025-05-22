26a1f10e7f196cbf8137d6af413c0a3a5d147486
mishashto <me@reze.moe>
2025-05-22 10:41:40 +0900

    docs: add static pages (faq and about)

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="central-content">
+      <h1>About</h1>
+      <p><i>aka history of the service</i></p>
+      <br>
+      <div class="slim-content">
+        <p>This is a fourth iteration of a file uploader, that was previously referred to as <NuxtLink to="https://github.com/GoofyGoofsterClub/imagination-server">ImaginationServer</NuxtLink>. It offered file uploads for ShareX users and Chatterino users, as well as online upload at a later update. While we still offer that, we now offer more variable file manipulations and faster uploads and load times for files.</p>
+        <p>It all started as a joke, when I, <NuxtLink to="https://mishashto.com">mishashto</NuxtLink>, bought a domain name that had female private parts in its name and .pics TLD and started uploading my images on it to share in Twitch chat, quickly after a lot of people wanted to have access to it and I was forced ot make a dumbed down NodeJS version with simple HTML and no CSS dashboard to allow people to control their uploads.</p>
+        <p>After a year or so, Saudi Arabian government banned access to the service inside the country, presumably because of the domain name. To resolve the issue quickly, I bought another domain, <b>no</b>[femalepart].pics, and hosted a mirror with all references to obscenity being <i>halal</i>ified, as well as submitted an appeal to Saudi government. In about a month or two the main domain was unbanned and the mirror was abandoned.</p>
+        <p>I added a lot of other domains as mirrors in the meantime, but the one that remained after the extremely expensive funny domain expired was <NuxtLink to="https://uwu.so">uwu.so</NuxtLink>. And with that all people moved to it and it stopped being a mirror.</p>
+        <p>Service was hosted on AWS but the pricing and servers provided by AWS were honestly not the best, so the servers were transferred to the <NuxtLink to="https://pavel.gg">NekoPavel's</NuxtLink> home server, for which I'm still really thankful! The fact that his home server is alive keeps this service running for everyone else for free <i>(except me, and him)</i>.</p>
+      </div>
+      <br>
+      <h2>Early contributors</h2>
+      <p>Thanks to doritorainn, <NuxtLink to="https://m.twitch.tv/nephraa/home">Nephraa</NuxtLink>, <NuxtLink to="https://github.com/clevernt/">clever(nt)</NuxtLink>, <NuxtLink to="https://x.com/kanuiosu">kanui</NuxtLink>, UYCactus, <NuxtLink to="https://pavel.gg">NekoPavel</NuxtLink>, <NuxtLink to="https://www.twitch.tv/kexiv_">Kexiv</NuxtLink> and others for initially using the service and testing it!</p>
+      <br>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/faq.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="central-content">
+      <h1>FAQ</h1>
+      <p><i>aka what i cant be asked to answer</i></p>
+      <br>
+      <h2>I forgot my key</h2>
+      <p>I am working on implementing resetting of keys without administrator contact, but as of now you have to contact one of the administrators to get a reset link.</p>
+      <h2>I found my copyrighted material on your website</h2>
+      <p>Please contact <NuxtLink to="mailto:me@reze.moe">me@reze.moe</NuxtLink> (me@reze.moe) with a <b>[DMCA]</b> in the subject. Attach any proof of ownership of the material and infringing links.</p>
+      <h2>Can I get access?</h2>
+      <p>If you have to ask, no.</p>
+      <h2>Why is it down or slow sometimes?</h2>
+      <p>Downtime is as rare as we can possibly get, excluding the daily morning restart (Sweden morning). Slowness may be due to Telia being morons or you're far away from Sweden. If you want to contribute to stability, please inquire with <NuxtLink to="https://pavel.gg">NekoPavel</NuxtLink> on how you can donate.</p>
+      <h2>Can I read Privacy Policy or Terms of Service?</h2>
+      <p>I use the same terms of service for all my projects. You can read them <NuxtLink to="https://mishashto.com/terms">here</NuxtLink>.</p>
+      <h2>Is this open-source?</h2>
+      <p>Yes. Since September 19th, 2023. Please look at <NuxtLink to="https://github.com/orgs/GoofyGoofsterClub/repositories">our GitHub organization's</NuxtLinK> repositories.</p>
+      <h2>Is there public API?</h2>
+      <p>Not yet.</p>
+      <h2>Is there file size limit?</h2>
+      <p>No, but do not upload all seasons of RuPaul's Drag Race. Humanity invented common sense for a reason.</p>
+      <h2>How do I set this up?</h2>
+      <p>For ShareX please open your dashboard and download the settings file and import it in ShareX. For Chatterino:</p>
+      <p>
+        <ul><li>Request URL: https://uwu.so/api/private/uploads/new</li>
+        <li>Form field: image</li>
+        <li>Extra headers: Authorization: YOURKEY</li>
+        <li>Image link: {data.link}</li></ul>
+      </p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
