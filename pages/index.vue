01ae9f4176a6450e32179702260f13958cada11e
mishashto <me@reze.moe>
2025-05-22 10:44:56 +0900

    chore: remove mention of it being my file uploader

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main-page-content">
-        <h1>mishashto's File Uploader</h1>
+        <h1>Really Cute File Uploader</h1>
         <p>Only way I know how to upload files now</p>
     </div>
 </template>
