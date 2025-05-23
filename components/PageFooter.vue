22636effd4819ff7ae34debb179c00dc0f74d2b8
mishashto <me@reze.moe>
2025-05-23 16:50:21 +0900

    chore: move variables to a different style file

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -6,8 +6,8 @@
     <NuxtLink href="#" draggable="false" class="right-aligned-button" @click.prevent="toggleDashboard" :class="{ active: isDashboardLoginPanelActive }">Dashboard</NuxtLink>
     <div class="dashboard-login-board" :class="{ active: isDashboardLoginPanelActive }">
       <div>
-        <input placeholder="Access Key" type="password" v-show="!isAuthContentHidden">
-        <button v-show="!isAuthContentHidden" @click="tryLogin">Auth</button>
+        <input placeholder="Access Key" type="password" :disabled="isAuthContentDisabled" v-show="!isAuthContentHidden">
+        <button v-show="!isAuthContentHidden" :disabled="isAuthContentDisabled" @click="tryLogin">Auth</button>
         <p v-show="isAuthContentHidden">{{ authErrorMessage }}</p>
       </div>
     </div>
@@ -19,6 +19,7 @@ import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const isDashboardLoginPanelActive = ref(false)
+const isAuthContentDisabled = ref(false)
 
 const toggleDashboard = () => {
   isDashboardLoginPanelActive.value = !isDashboardLoginPanelActive.value
@@ -35,8 +36,18 @@ const authErrorMessage = ref('')
 
 const tryLogin = async () =>
 {
+  isAuthContentDisabled.value = true;
+}
+
+function setLoginError(errorMessage)
+{
   isAuthContentHidden.value = true
-  authErrorMessage.value = 'Example error message.'
+  authErrorMessage.value = errorMessage
+
+  setTimeout(() => {
+    authErrorMessage.value = ""
+    isAuthContentHidden.value = false
+  }, 2222);
 }
 </script>
 
