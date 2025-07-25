b485ca9525211ff8ce74301063a6a648939d5c9c
LMNYX <me@reze.moe>
2025-07-25 17:12:24 +0500

    feat(api): add invite model

---
+++ api/src/models/invite.py
@@ -0,0 +1,14 @@
+import hashlib
+from tortoise import fields
+from tortoise.models import Model
+from src.models.user import User
+
+class Invite(Model):
+    id = fields.IntField(pk=True, generated=True, unique=True, null=False)
+    inviter = fields.ForeignKeyField("models.User", related_name="invites")
+    username = fields.CharField(max_length=255)
+    inviation_code = fields.CharField(max_length=255)
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    class Meta:
+        table = "invites"
