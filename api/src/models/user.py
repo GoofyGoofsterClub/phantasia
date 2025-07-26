8d3acdd921927e48582a0c30eebcddb24580ce6c
LMNYX <me@reze.moe>
2025-07-26 13:51:23 +0500

    feat(api): add more functions to user model for ease of access

--- api/src/models/user.py
+++ api/src/models/user.py
@@ -1,4 +1,5 @@
 import hashlib
+import uuid
 from tortoise import fields
 from tortoise.models import Model
 
@@ -15,13 +16,43 @@ class User(Model):
     upload_count = fields.IntField(default=0)
     twitch_user_id = fields.CharField(max_length=255, null=True)
 
+    @classmethod
+    async def create_with_token(cls, **kwargs):
+        raw_token = str(uuid.uuid4())
+        token_hash = hashlib.sha256(raw_token.encode()).hexdigest()
+        user = await cls.create(access_key=token_hash, **kwargs)
+        return user, raw_token
+
     def encrypt_access_key(self):
         sha_signature = hashlib.sha256(self.access_key.encode()).hexdigest()
         return sha_signature
-    
+
     def compare_access_key(self, access_key):
         sha_signature = hashlib.sha256(access_key.encode()).hexdigest()
         return sha_signature == self.access_key
+    
+    def has_permission(self, permission):
+        return (permission in self.permissions
+            or 'SUPERUSER' in self.permissions
+            or 'ADMINISTRATOR' in self.permissions)
+
+    @classmethod
+    async def exists_by_access_key(cls, access_key: str) -> bool:
+        hashed_key = hashlib.sha256(access_key.encode()).hexdigest()
+        return await cls.filter(access_key=hashed_key).exists()
+
+    @classmethod
+    async def find_by_id(cls, user_id: int):
+        return await cls.get(id=user_id)
+
+    @classmethod
+    async def find_by_username(cls, username: str):
+        return await cls.get(username=username)
+
+    @classmethod
+    async def find_by_access_key(cls, access_key: str):
+        hashed_key = hashlib.sha256(access_key.encode()).hexdigest()
+        return await cls.get(access_key=hashed_key)
 
     class Meta:
         table = "users"
