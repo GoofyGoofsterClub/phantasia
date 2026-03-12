8021c8c0c9db98bf8e2ef544659727a4b3c34066
Mikhail <me@mishashto.com>
2026-03-13 01:35:30 +0500

    fix(frontend): area chart in dashboard caused 500 to popup on direct dashboard access

--- frontend/pages/dashboard/index.vue
+++ frontend/pages/dashboard/index.vue
@@ -25,17 +25,19 @@
                 <p>Your statistics</p>
                 <div class="delimiter"></div>
               </div>
-              <AreaChart
-                :data="AreaChartData"
-                :height="220"
-                :categories="categories"
-                :y-grid-line="true"
-                :x-formatter="xFormatter"
-                :x-num-ticks="4"
-                :y-num-ticks="4"
-                :curve-type="CurveType.MonotoneX"
-                :legend-position="LegendPosition.Top"
-              />
+              <ClientOnly>
+                <AreaChart
+                  :data="AreaChartData"
+                  :height="220"
+                  :categories="categories"
+                  :y-grid-line="true"
+                  :x-formatter="xFormatter"
+                  :x-num-ticks="4"
+                  :y-num-ticks="4"
+                  :curve-type="CurveType.MonotoneX"
+                  :legend-position="LegendPosition.Top"
+                />
+              </ClientOnly>
             </div>
             </div>
           </div>
@@ -76,6 +78,12 @@ const handleFileUpload = async (event: Event) => {
 
   const { apiBase } = useApiUrl()
   try {
+    const isValid = await authStore.verifySession()
+    if (!isValid) {
+      router.push('/')
+      return
+    }
+
     const response = await $fetch(`${apiBase}/uploads/create`, {
       method: 'POST',
       body: formData,
@@ -97,171 +105,51 @@ const handleFileUpload = async (event: Event) => {
 // chart
 interface AreaChartItem {
   date: string
-  Requests: number
-  ['Errors']: number
+  Uploads: number
+  ['Views']: number
 }
 
 const categories: Record<string, BulletLegendItemInterface> = {
-  Requests: { name: 'Uploads', color: '#4B6A88' },
-  Errors: { name: 'Views', color: '#fb2c36' },
+  Uploads: { name: 'Uploads', color: '#332cf9' },
+  Views: { name: 'Views', color: '#fb2c36' },
 }
 
 const AreaChartData: AreaChartItem[] = [
   {
-    date: 'Aug 01',
-    Requests: 1040,
-    Errors: 0,
+    date: 'Jan 1, 1970',
+    Uploads: 0,
+    Views: 0,
   },
   {
-    date: 'Aug 02',
-    Requests: 1200,
-    Errors: 0,
+    date: 'Jan 2, 1970',
+    Uploads: 0,
+    Views: 0,
   },
   {
-    date: 'Aug 03',
-    Requests: 1130,
-    Errors: 0,
+    date: 'Jan 3, 1970',
+    Uploads: 0,
+    Views: 0,
   },
   {
-    date: 'Aug 04',
-    Requests: 1050,
-    Errors: 0,
+    date: 'Jan 4, 1970',
+    Uploads: 0,
+    Views: 0,
   },
   {
-    date: 'Aug 05',
-    Requests: 920,
-    Errors: 0,
+    date: 'Jan 5, 1970',
+    Uploads: 0,
+    Views: 0,
   },
   {
-    date: 'Aug 06',
-    Requests: 870,
-    Errors: 0,
+    date: 'Jan 6, 1970',
+    Uploads: 0,
+    Views: 0,
   },
   {
-    date: 'Aug 07',
-    Requests: 790,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 08',
-    Requests: 910,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 09',
-    Requests: 951,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 10',
-    Requests: 1232,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 11',
-    Requests: 1230,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 12',
-    Requests: 1289,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 13',
-    Requests: 1002,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 14',
-    Requests: 1034,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 15',
-    Requests: 1140,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 16',
-    Requests: 1280,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 17',
-    Requests: 1345,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 18',
-    Requests: 1432,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 19',
-    Requests: 1321,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 20',
-    Requests: 1230,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 21',
-    Requests: 1020,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 22',
-    Requests: 1040,
-    Errors: 0,
-  },
-  {
-    date: 'Aug 23',
-    Requests: 610,
-    Errors: 81,
-  },
-  {
-    date: 'Aug 24',
-    Requests: 610,
-    Errors: 87,
-  },
-  {
-    date: 'Aug 25',
-    Requests: 610,
-    Errors: 92,
-  },
-  {
-    date: 'Aug 26',
-    Requests: 501,
-    Errors: 120,
-  },
-  {
-    date: 'Aug 27',
-    Requests: 480,
-    Errors: 120,
-  },
-  {
-    date: 'Aug 28',
-    Requests: 471,
-    Errors: 120,
-  },
-  {
-    date: 'Aug 29',
-    Requests: 610,
-    Errors: 89,
-  },
-  {
-    date: 'Aug 30',
-    Requests: 513,
-    Errors: 199,
-  },
-  {
-    date: 'Aug 31',
-    Requests: 500,
-    Errors: 56,
-  },
+    date: 'Jan 7, 1970',
+    Uploads: 0,
+    Views: 0,
+  }
 ]
 const xFormatter = (i: number): string | number => `${AreaChartData[i]?.date}`
 const sum = (arr, key) => arr.reduce((sum, obj) => sum + obj[key], 0)
